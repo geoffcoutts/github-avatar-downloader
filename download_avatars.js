cb5b1d2d989d6471f9f3f9dccd0e25e325fb6ee9
Geoff Coutts <geoff.coutts@gmail.com>
2018-02-19 23:43:52 +0000

    Avatars will be saved to a directory with a name that matches the repo name

--- download_avatars.js
+++ download_avatars.js
@@ -5,6 +5,9 @@ var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  // if (process.argv[2] || process.argv[3] === "") {
+  //   return console.log('Please try again and enter a valid repo owner and name');
+  // }
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
@@ -13,16 +16,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-    request(options, function(err, res, body) {
-      var data = JSON.parse(body);
-      cb(err, data);
-    });
+  request(options, function(err, res, body) {
+    var data = JSON.parse(body);
+    cb(err, data);
+  });
 }
 
+var input = process.argv.slice(2,4);
+
 getRepoContributors("jquery", "jquery", function(err, result) {
+  // loop through avatar results and pull the avatar_url to download
+  console.log(input[1]);
+  fs.existsSync(`${input[1]}-avatars`) || fs.mkdirSync(`${input[1]}-avatars`);
   for (i = 0; i < result.length; i++) {
     console.log("Downloading: ", result[i].avatar_url);
-    downloadImageByURL(result[i].avatar_url, `avatars/${result[i].login}.jpg`);
+    // Download avatars and save to folder with the name of the repository
+    downloadImageByURL(result[i].avatar_url, `${input[1]}-avatars/${result[i].login}.jpg`);
     console.log("Finished downloading.");
   }
 });
